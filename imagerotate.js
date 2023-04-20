bb35df850ed165591047c8a323009989d8d3c3b8
Jan Youssef <janyoussef19@gmail.com>
2023-04-20 13:59:55 +0200

    functioality completed

--- imagerotate.js
+++ imagerotate.js
@@ -1,23 +1,35 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     var changer = document.getElementById("color-changer-id");
     var image = document.getElementById("rotating-img-id");
+    var text = document.getElementById("title-text");
+    
     changer.addEventListener("mouseenter", e => enter(e))
     changer.addEventListener("mouseleave", leave)
 
 
     function enter(event) {
-        changer.addEventListener("mousemove", e => move(e))
+        changer.addEventListener("mousemove", e => move(e));
+        text.innerText = "What I am Actually listnening to:";
     }
     function leave() {
+        //reset image state
         image.style.rotate = "initial";
         image.style.scale = "initial";
         image.style.transform = "initial";
+        image.setAttribute("src","jan_coding.png");
+
+        //reset text
+        text.innerText = "How I look like when coding:";
+        //remove event handler
         changer.removeEventListener("mousemove", move, true);
     }
     function move(e) {
+        //change image
+        image.setAttribute("src","jan.jpeg");
+
+        //rotate and scale
         const rotation = ((e.clientX + e.clientY) % 255);
         image.style.rotate = ""+rotation+"deg";
-
         const scale = e.clientY / window.innerWidth * 5;
         image.style.transform = `scale(${scale})`;
     }
